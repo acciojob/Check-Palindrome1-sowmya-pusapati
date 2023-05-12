18efe6872942b76f00b618bf635fc5d6ef9b78cb
Sowmya Bharathi Pusapati <119491409+sowmya-pusapati@users.noreply.github.com>
2023-05-12 11:39:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
+	let n=str.length;
+	let i=0;
+	let j=n-1;
+	while(i<=j)
+		{
+			if(str.charAt(i)!=str.charAt(j))
+			{
+				return false;
+			}
+			i++;j--;
+		}
+	return true;
 
 }
 module.exports = palindrome
